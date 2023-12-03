7c8c12106f732b6060aef654f55f65a34018643c
DanielScheffel <danioliveira.8292@gmail.com>
2023-12-03 14:24:03 -0300

    Mexendo nas views e na estilização da página

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from contact.models import Contact
 
+# \ para não deixar a linha ficar muito estença
 
 def index(request):
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .all().order_by('-id')[:10]
+    
+    # print(contacts.query)
+    
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
         'contact/index.html',
+        context,
     )
